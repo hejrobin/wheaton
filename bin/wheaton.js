4c42e43e889907070e79fec29b3046f787a7e6dc
Robin Grass <hej@carbin.se>
2015-09-21 17:11:27 +0200

    Expose classes to module.

--- bin/wheaton.js
+++ bin/wheaton.js
@@ -1,14 +1,23 @@
-var Wheaton;
+var Wheaton, path;
+
+path = require('path');
 
 Wheaton = (function() {
   function Wheaton() {}
 
   Wheaton["import"] = function(wheatonPackage) {
-    return require("./" + (wheatonPackage.toLowerCase()));
+    console.warn('Deprecation warning: Objects are exposed to Wheaton.');
+    return require(path.join(__dirname, wheatonPackage));
   };
 
   return Wheaton;
 
 })();
 
-module.exports = Wheaton;
+module.exports["import"] = Wheaton["import"];
+
+module.exports.Storage = require('./data/storage');
+
+module.exports.Deck = require('./objects/deck');
+
+module.exports.Card = require('./objects/card');
--- src/vendor/wheaton/wheaton.coffee
+++ src/vendor/wheaton/wheaton.coffee
@@ -1,7 +1,13 @@
+path                      = require 'path'
+
 class Wheaton
 
   @import: (wheatonPackage) ->
-    require "./#{wheatonPackage.toLowerCase()}"
+    console.warn 'Deprecation warning: Objects are exposed to Wheaton.'
+    require path.join(__dirname, wheatonPackage)
 
 
-module.exports = Wheaton
+module.exports.import = Wheaton.import
+module.exports.Storage = require './data/storage'
+module.exports.Deck = require './objects/deck'
+module.exports.Card = require './objects/card'
